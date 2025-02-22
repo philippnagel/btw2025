25bd15a7840feaf5ab73bc0a355beed572a3a52f
philippnagel <phil@philippnagel.com>
2025-02-22 17:46:48 +0100

    Fix deps

--- postprocess.ts
+++ postprocess.ts
@@ -1,5 +1,4 @@
-import { readCSV } from 'https://deno.land/x/flat@0.0.15/mod.ts';
-import * as fs from 'https://deno.land/std@1.0.13/fs/mod.ts'; 
+import { readCSV, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
 
 const csvPath = './data.csv';
 const jsonPath = './data.json';
@@ -8,7 +7,9 @@ try {
   const originalCSV = await readCSV(csvPath);
   console.log(originalCSV);
 
-  await fs.writeJson(jsonPath, originalCSV, { spaces: 2 });
+  const jsonString = JSON.stringify(originalCSV, null, 2);
+
+  await Deno.writeTextFile(jsonPath, jsonString);
   console.log(`Data has been written to ${jsonPath}`);
 } catch (error) {
   console.error('Error processing CSV to JSON:', error);
